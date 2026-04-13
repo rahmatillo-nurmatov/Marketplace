a8bb09bb7bccab854068e4de0c19f30761a85db2
rahmatillo-nurmatov <nurmatovr008@icloud.com>
2026-04-14 03:00:21 +0600

    fix

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -15,7 +15,7 @@ export default function HistoryPage() {
 
   useEffect(() => {
     if (user?.uid) {
-      orderService.getUserOrders(user.uid)
+      orderService.getOrdersByClient(user.uid)
         .then(data => {
           if (Array.isArray(data)) setOrders(data);
           setLoading(false);
